a77bd88dc5d939b6c8b5811b5d3c6de1be5e0dfa
Kumari Sweta <48363292+Swetagupta647@users.noreply.github.com>
2020-08-19 19:01:45 +0530

    Add files via upload

---
+++ discussionforum2/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Discussionforum2Item(scrapy.Item):
+    # define the fields for your item here like:
+    # author = scrapy.Field()
+    # title = scrapy.Field()
+    # like = scrapy.Field()
+    # link = scrapy.Field()
+    # sequence = scrapy.Field()
+    # date = scrapy.Field()
+    # user = scrapy.Field()
+    # comment = scrapy.Field()
+    pass
